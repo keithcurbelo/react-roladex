17a4e77efa8c2ecc30d747eb85e1cfa3aa5b3ebb
Keith Curbelo <keithcurbelo@yahoo.com>
2022-04-30 09:11:49 -0400

    add api call and lifecycle comments

--- src/App.js
+++ src/App.js
@@ -3,12 +3,29 @@ import { Component } from 'react';
 import './App.css';
 
 class App extends Component {
+	/** Runs first
+	 *  Used to initialize state
+	 */
 	constructor() {
+		//Always runs first and used to initialize state
 		super();
 		this.state = {
-			monsters: [{ name: 'linda' }, { name: 'Frank' }, { name: 'Jackie' }],
+			monsters: [],
 		};
 	}
+	/** Runs third, after render()
+	 * First time component is mounted to DOM.
+	 */
+	componentDidMount() {
+		fetch('https://jsonplaceholder.typicode.com/users')
+			.then((resp) => resp.json())
+			.then((users) => this.setState(() => ({ monsters: users }))); //executes re-render and calls render() method again
+	}
+
+	/** Runs second, after constructor()
+	 * Initial rendering and mounting of component on to DOM.
+	 * Gets called every time a render/re-render needs to occur
+	 */
 	render() {
 		return (
 			<div className='App'>
