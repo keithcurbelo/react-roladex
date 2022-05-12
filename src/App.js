772bc26929b24ca6cf53c0a9c74ca1cff92c7be1
Keith Curbelo <keithcurbelo@yahoo.com>
2022-05-12 08:20:13 -0400

    fix searchbox class prop

--- src/App.js
+++ src/App.js
@@ -46,6 +46,7 @@ class App extends Component {
 		return (
 			<div className='App'>
 				<SearchBox
+					className='search-box'
 					onChangeHandler={onSearchChange}
 					placeholder='search monsters'
 				/>
