c090016ef1a85a7934432a58936fbabcf5f3c8b2
Keith Curbelo <keithcurbelo@yahoo.com>
2022-05-12 08:55:54 -0400

    add elements and styling to card list

--- src/components/card-list/card-list.component.jsx
+++ src/components/card-list/card-list.component.jsx
@@ -1,13 +1,21 @@
 import { Component } from 'react';
+import './card-list.styles.css';
 
 class CardList extends Component {
 	render() {
 		const { items } = this.props;
 		return (
-			<div>
-				{items.map((item, i) => (
-					<h1 key={i}>{item.name}</h1>
-				))}
+			<div className='card-list'>
+				{items.map((item, i) => {
+					const { id, name, email } = item;
+					return (
+						<div className='card-container' key={id}>
+							<img src={`https://robohash.org/${id}?set=set4`} alt={name} />
+							<h2>{name}</h2>
+							<p>{email}</p>
+						</div>
+					);
+				})}
 			</div>
 		);
 	}
