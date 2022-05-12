4bd089c0982ca9587f6bea9a085c85b6ec4c041f
Keith Curbelo <keithcurbelo@yahoo.com>
2022-05-12 09:19:49 -0400

    add app title import new font and style app title

--- src/App.js
+++ src/App.js
@@ -45,6 +45,7 @@ class App extends Component {
 
 		return (
 			<div className='App'>
+				<h1 className='app-title'>Monsters Roladex</h1>
 				<SearchBox
 					className='search-box'
 					placeholder='search monsters'
