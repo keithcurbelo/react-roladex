ef4133bca26915383722a964563caaa04f3fc097
Keith Curbelo <keithcurbelo@yahoo.com>
2022-05-12 09:18:58 -0400

    apply card component to card list update css and add component reference

--- src/components/card-list/card-list.component.jsx
+++ src/components/card-list/card-list.component.jsx
@@ -1,4 +1,5 @@
 import { Component } from 'react';
+import Card from '../card/card.component';
 import './card-list.styles.css';
 
 class CardList extends Component {
@@ -7,14 +8,7 @@ class CardList extends Component {
 		return (
 			<div className='card-list'>
 				{items.map((item, i) => {
-					const { id, name, email } = item;
-					return (
-						<div className='card-container' key={id}>
-							<img src={`https://robohash.org/${id}?set=set4`} alt={name} />
-							<h2>{name}</h2>
-							<p>{email}</p>
-						</div>
-					);
+					return <Card key={i} {...item} />;
 				})}
 			</div>
 		);
